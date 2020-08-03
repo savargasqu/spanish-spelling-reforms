f8202091670ff1892cd8e69b6948603f99f157ec
Sergio Alejandro Vargas <savargasqu@unal.edu.co>
2020-08-02 23:27:54 -0500

    Add more patterns

--- transliterator.jl
+++ transliterator.jl
@@ -1,27 +1,43 @@
 #=
 [Replace function
 ](https://docs.julialang.org/en/v1/base/strings/#Base.replace-Tuple{AbstractString,Pair})
-
-The replace function takes a pair, where the value can be a substitution string
-or a function
 =#
+
+# This function works on strings *not* chars
+subkeepcase(old, new) = isuppercase(old[1]) ? uppercase(new) : lowercase(new)
 
 function transliterate()
     teststr = """El rey dice: 'Yo SOY la ley'.
                  El bufón dice: Ahí hay chocolate"""
-
     # *y -> *i
-    # Use a lookahead to know if the 'y' is at the end of a word
-    # FIXME: Find a cleaner case-preserving substitution.
-    #        This expression is too long…
-    ypat = r"(y|Y)(?=\b)" => x -> isuppercase(x[1]) ? "I" : "i"
-    resstr = replace(teststr, ypat)
-    # h* -> Ø
-    # Use a lookbehind to check that the 'h' is not preceded by a 'c'
-    # The "ch" case should be handled differently
-    hpat = r"(?<=[^c])h|H" => ""
-    resstr = replace(resstr, hpat)
-    resstr
+    # Which of this is better?
+    pattern_y = r"(y|Y)(?=\b)" => y -> subkeepcase(y, "i")
+
+    pattern_y = r"i(?=\b)" => "i"
+    pattern_Y = r"Y(?=\b)" => "I"
+
+    teststr = replace(teststr, pattern_y)
+
+    # // h -> Ø. The "ch" case should be handled differently
+    h = r"(?<=[^c])h|H" => ""
+    # /g/ gu{ei} -> g{ei}; gü -> gu
+    g = r"gu(?=[eéií])" => "g"
+    diaeresis = r"ü" => "u"
+    # /k/ C, QU -> K
+    ck =  r"c(?=[aáoóuú])" => "k"
+    q = r"qu" => "k"
+    # x = "x" => "ks" # More complicated than this
+    # /s/ Z, C -> S (seseo)
+    c = r"c(?=[eéií])" => "s"
+    z = "z" => "s"
+    # /θ/ C => Z (distinción)
+    c = r"c(?=[eéií])" => "z"
+    # /x/ G, J -> J
+    g = r"g(?=[eéií])" => "j"
+    # /j/ LL -> Y (yeísmo)
+    ll = "ll" => "y"
+    # /β/ B -> V (betacismo)
+    b = "v" => "b"
 end
 
 println("$(transliterate())")
