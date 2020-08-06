a6d86b67ab220093af72adddc3158fe8a935217a
Sergio Alejandro Vargas <savargasqu@unal.edu.co>
2020-08-05 20:40:21 -0500

    Modify letter case function, split file function
    Change the keepcase function to work on chars, not strings.
    Note: Working on chars massively reduces allocations,
    
    Move the proper transliteration out of the read_and_transliterate
    function.
    Note: Spliting the file function also helps with performance.
    
    TODO:
    * keepcase now doesn't work for the `h` pattern.
    * The whole file is still loaded at once. should it be done line by
      line?

--- transliterator.jl
+++ transliterator.jl
@@ -1,37 +1,38 @@
-
+# Using chars reduces allocations?
 const spellpatterns = [
-    (r"y(?=\b)"i      , "i") # /i/  *y  -> *i
-    (r"(?<=[^c])h"i   , "" ) # / /  h   -> Ø
-    (r"c(?=[^eéiíh])"i, "k") # /k/  C   -> K
-    (r"qu"i           , "k") # /k/  QU  -> K
-    (r"gu(?=[eéií])"i , "g") # /g/  gu* -> g*
-    (r"ü"i            , "u") # /gu/ gü  -> gu
-    (r"g(?=[eéií])"i  , "j") # /x/  G   -> J
-    (r"c(?=[eéií])"i  , "s") # /s/  C   -> S (seseo)
-    (r"z"i            , "s") # /s/  Z   -> S (seseo)
-    #(r"v"i            , "b") # /β/  V   -> B (betacismo)
-    #(r"ll"i           , "y") # /j/  LL  -> Y (yeísmo)
-    #(r"c(?=[eéií])"i  , "z") # /θ/  C   -> Z (distinción)
+    (r"y(?=\b)"i      , 'i') # /i/  *y  -> *i
+    #(r"(?<=[^c])h"i   , "")  # / /  h   -> Ø. How to handle this with chars?
+    (r"c(?=[^eéiíh])"i, 'k') # /k/  C   -> K
+    (r"qu"i           , 'k') # /k/  QU  -> K
+    (r"gu(?=[eéií])"i , 'g') # /g/  gu* -> g*
+    (r"ü"i            , 'u') # /gu/ gü  -> gu
+    (r"g(?=[eéií])"i  , 'j') # /x/  G   -> J
+    (r"c(?=[eéií])"i  , 's') # /s/  C   -> S (seseo)
+    (r"z"i            , 's') # /s/  Z   -> S (seseo)
+    (r"ll"i           , 'y') # /j/  LL  -> Y (yeísmo)
 ]
+    #(r"v"i            , 'b') # /β/  V   -> B (betacismo)
+    #(r"c(?=[eéií])"i  , 'z') # /θ/  C   -> Z (distinción)
 
-# This function works on strings *not* chars
-keepcase(old, new) = isuppercase(old[1]) ? uppercase(new) : lowercase(new)
+keepcase(old, new)::Char = isuppercase(old) ? uppercase(new) : new
 
 function transliterate(str)
     for p in spellpatterns
-        str = replace(str, p[1] => x -> keepcase(x, p[2]))
+        str = replace(str, p[1] => x -> keepcase(x[1], p[2]))
     end
     str
 end
 
-function read_and_transliterate(infilename, outfilename)
-    infile = open(infilename)
-    open(infilename) do infile
-        open(outfilename, "w") do outfile
-            write(outfile, transliterate(read(infile, String)))
-        end
+# Should this read the file line by line instead?
+function transliteratefile(f::IOStream)
+    return transliterate(read(f, String)) 
+end
+
+function read_and_transliterate(infname, outfname)
+    t = open(infname) do infile
+        transliteratefile(infile)
     end
+    write(outfname, t)
 end
 
 read_and_transliterate("test/input.txt", "test/output.txt")
-
