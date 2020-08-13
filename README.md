adc14478714c53f8953066b35a4ade68736ae05d
Sergio Alejandro Vargas <savargasqu@unal.edu.co>
2020-08-13 15:33:56 -0500

    Remove readme and dummytest

--- README.md
+++
@@ -1,9 +0,0 @@
-# Spanish spelling reforms
-
-A script to respell Spanish text into multiple alternative orthographies.
-Work in progress.
-
-TODO:
-  - Escape words beginning with a backslash, for LaTeX
-  - Revise old essay, and make a notebook
-
--- dummytest.jl
+++
@@ -1,8 +0,0 @@
-# This isn't a real unit test
-include("src/transcriber.jl")
-using Main.Transcriber
-
-transcribefile("bello",   "examples/derechos-humanos.txt", "examples/output-bello.txt")
-transcribefile("korreas", "examples/derechos-humanos.txt", "examples/output-korreas.txt")
-transcribefile("vallejo", "examples/derechos-humanos.txt", "examples/output-vallejo.txt")
-
